7001e5a09a74fff772adefdf6238cb47f26221f1
mmaviglia <mason.maviglia@gmail.com>
2025-01-10 00:23:55 -0800

    implement package foundation

---
+++ audioplayer.go
@@ -0,0 +1,150 @@
+package audioplayer
+
+import (
+	"context"
+	"io"
+	"log"
+	"os/exec"
+	"strconv"
+	"sync"
+
+	"github.com/hajimehoshi/oto"
+)
+
+type AudioPlayer struct {
+	mu sync.Mutex
+	wg sync.WaitGroup
+
+	cancelPlayback context.CancelFunc
+	ffmpegCmd      *exec.Cmd
+	player         *oto.Player
+	context        *oto.Context
+	audioFile      string
+
+	speed     float64 // must be between 0.5 and 2.0
+	startTime int     // in seconds
+}
+
+func NewAudioPlayer(audioFile string, speed float64, startTime int) *AudioPlayer {
+	return &AudioPlayer{
+		audioFile: audioFile,
+		speed:     speed,
+		startTime: startTime,
+	}
+}
+
+// Start creates an FFmpeg process to decode the audio file into raw PCM data and
+// creates an Oto player to play the audio. This should be called to start audio playback.
+func (ap *AudioPlayer) Start() error {
+	ap.mu.Lock()
+	defer ap.mu.Unlock()
+
+	if ap.ffmpegCmd != nil {
+		ap.mu.Unlock()
+		ap.Close()
+		ap.mu.Lock()
+	}
+
+	ffmpegPath := "./tools/ffmpeg" // TODO: Replace with config path
+
+	// FFmpeg command to decode audio into raw PCM
+	ffmpegCmd := exec.Command(
+		ffmpegPath,
+		"-ss", strconv.Itoa(ap.startTime),
+		"-i", ap.audioFile,
+		"-filter:a", "atempo="+strconv.FormatFloat(ap.speed, 'f', -1, 64),
+		"-f", "s16le",
+		"-ar", "44100",
+		"-ac", "2",
+		"pipe:1",
+	)
+
+	ffmpegOut, err := ffmpegCmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	ap.cancelPlayback = cancel
+	reader := NewReader(ctx, ffmpegOut)
+
+	if err := ffmpegCmd.Start(); err != nil {
+		return err
+	}
+
+	// Create Oto context and player if not already initialized
+	if ap.context == nil {
+		bufferSizeInBytes := 4 * 1024
+
+		context, err := oto.NewContext(44100, 2, 2, bufferSizeInBytes)
+		if err != nil {
+			log.Fatalf("Failed to create Oto context: %v", err)
+		}
+		ap.context = context
+	}
+
+	ap.wg.Add(1)
+	ap.player = ap.context.NewPlayer()
+	go func() {
+		if _, err := io.Copy(ap.player, reader); err != nil {
+			log.Printf("Error copying audio data: %v", err)
+		}
+		ap.wg.Done()
+		ap.Stop()
+	}()
+
+	ap.ffmpegCmd = ffmpegCmd
+	return nil
+}
+
+// Stop closes any active oto.Player and kills any active FFmpeg process. This should be
+// called to pause or stop audio playback.
+func (ap *AudioPlayer) Stop() {
+	ap.mu.Lock()
+	defer ap.mu.Unlock()
+
+	// Cancel the context to stop the goroutine
+	if ap.cancelPlayback != nil {
+		ap.cancelPlayback()
+	}
+
+	// Wait for the goroutine to finish
+	ap.wg.Wait()
+
+	if ap.ffmpegCmd != nil {
+		_ = ap.ffmpegCmd.Process.Kill()
+		_ = ap.ffmpegCmd.Wait()
+		ap.ffmpegCmd = nil
+	}
+
+	if ap.player != nil {
+		ap.player.Close()
+		ap.player = nil
+	}
+}
+
+// Close stops the audio player and closes the oto.Context.
+func (ap *AudioPlayer) Close() {
+	ap.Stop()
+	if ap.context != nil {
+		ap.context.Close()
+		ap.context = nil
+	}
+}
+
+type readerCtx struct {
+	ctx context.Context
+	r   io.Reader
+}
+
+func (r *readerCtx) Read(p []byte) (n int, err error) {
+	if err := r.ctx.Err(); err != nil {
+		return 0, err
+	}
+	return r.r.Read(p)
+}
+
+// NewReader gets a context-aware io.Reader.
+func NewReader(ctx context.Context, r io.Reader) io.Reader {
+	return &readerCtx{ctx: ctx, r: r}
+}
